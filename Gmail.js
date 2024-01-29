e0f4f3410a78c5246951ea5896e35fac7a11147f
febin thomas <tfebin39@gmail.com>
2024-01-29 23:06:51 +0530

    first commit

--- Gmail.js
+++ Gmail.js
@@ -1,19 +1,55 @@
-function SendMail(){
-    var params={  
-         from_name :document.getElementById("from_name").value,
-         last_name :document.getElementById("LastName").value,
-            email_id : document.getElementById("email_id").value
+
+
+
+
+function SendMail() {
+    const from_name = document.getElementById("from_name").value;
+    const LastName = document.getElementById("LastName");
+    const email_id = document.getElementById("email_id");
+
+
+
+    if (from_name === "" || LastName === "" || email_id === "") {
+
+        document.querySelector(".alert").style.display = "block";
+        
+
+
+  //   remove the alert
+     setTimeout(() => {
+    document.querySelector(".alert").style.display = "none";
+    document.querySelector(".success").style.display = "none";
+
+  }, 3000);
+    }
+    else {
+
+
+        document.querySelector(".success").style.display = "block";
+        setTimeout(() => {
+            document.querySelector(".success").style.display = "none";
+        
+          }, 3000);
+        var params = {
+            from_name: document.getElementById("from_name").value,
+            last_name: document.getElementById("LastName").value,
+            email_id: document.getElementById("email_id").value
 
         }
-        emailjs.send("service_0ti6tap","template_5tey5xi",params).then(
-            res =>{
+        emailjs.send("service_0ti6tap", "template_5tey5xi", params).then(
+            res => {
 
-                document.getElementById("from_name").value="";
-                document.getElementById("LastName").value="";
-                document.getElementById("email_id").value="";
+                document.getElementById("from_name").value = "";
+                document.getElementById("LastName").value = "";
+                document.getElementById("email_id").value = "";
 
                 alert("Success!");
-            
-        }
+
+            }
         )
-};
+        console.log("done")
+    }
+
+}
+
+
