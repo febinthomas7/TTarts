6deffa6f142077324db8cb7677b859e574688f81
febin thomas <tfebin39@gmail.com>
2024-02-11 14:03:14 +0530

    minor fixes

--- Gmail.js
+++ Gmail.js
@@ -12,20 +12,22 @@ function SendMail() {
       document.querySelector(".success").style.display = "none";
     }, 3000);
   } else {
+    var params = {
+      First_Name: document.getElementById("from_name").value,
+      Last_Name: document.getElementById("LastName").value,
+      Email_id: document.getElementById("email_id").value,
+    };
     emailjs.send("service_0ti6tap", "template_5tey5xi", params).then((res) => {
-      document.getElementById("from_name").value = "";
-      document.getElementById("LastName").value = "";
-      document.getElementById("email_id").value = "";
+      setTimeout(() => {
+        document.getElementById("from_name").value = "";
+        document.getElementById("LastName").value = "";
+        document.getElementById("email_id").value = "";
+      }, 1000);
     });
 
     document.querySelector(".success").style.display = "block";
     setTimeout(() => {
       document.querySelector(".success").style.display = "none";
     }, 3000);
-    var params = {
-      from_name: document.getElementById("from_name").value,
-      last_name: document.getElementById("LastName").value,
-      email_id: document.getElementById("email_id").value,
-    };
   }
 }
